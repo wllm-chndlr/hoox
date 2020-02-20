af85fd1520b78eefdce9fa7581199f806c13ed4b
William Chandler <chndlr.wllm@gmail.com>
2020-02-19 21:14:47 -0600

    Updates

---
+++ precache-manifest.d7d3859f7415d6e7d73a6f9f4fbb7b33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8147f0a49b8273195be359740abb4f4",
+    "url": "/hoox/index.html"
+  },
+  {
+    "revision": "35a201fcaf03907e3350",
+    "url": "/hoox/static/css/main.2a4d028d.chunk.css"
+  },
+  {
+    "revision": "e921ce268fd6ab34c281",
+    "url": "/hoox/static/js/2.a3a0d95c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hoox/static/js/2.a3a0d95c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35a201fcaf03907e3350",
+    "url": "/hoox/static/js/main.5928a0b4.chunk.js"
+  },
+  {
+    "revision": "544fa9403732bf9d5c58",
+    "url": "/hoox/static/js/runtime-main.2b208d33.js"
+  }
+]);
